da674c1efce55bd94a8b245f0c864af080294c07
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2024-01-31 20:02:42 +0100

    Add mongodb setup and create connection to server

---
+++ backend/config/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+  let DB_URI = "";
+
+  switch (process.env.NODE_ENV) {
+    case "development":
+      DB_URI = process.env.DB_LOCAL_URI!;
+      break;
+    case "production":
+      DB_URI = process.env.DB_URI!;
+      break;
+
+    default:
+      DB_URI = process.env.DB_LOCAL_URI!;
+      break;
+  }
+
+  await mongoose
+    .connect(DB_URI)
+    .then(() => console.log("DB connected successfully"));
+};
+
+export default dbConnect;
