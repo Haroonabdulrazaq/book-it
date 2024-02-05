59405215b8c880c4f552339e0d6d323b7a56ad4f
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2024-02-05 21:25:54 +0100

    Add Complete CRUD for room COntroller

--- app/api/rooms/[id]/route.ts
+++ app/api/rooms/[id]/route.ts
@@ -1,20 +1,27 @@
 import dbConnect from "@/backend/config/dbConnect";
-import { allRooms, newRoom } from "@/backend/controllers/roomController";
+import {
+  getRoomDetails,
+  updateRoom,
+} from "@/backend/controllers/roomController";
 import { createEdgeRouter } from "next-connect";
 import { NextRequest } from "next/server";
 
-interface RequestContext {}
+interface RequestContext {
+  params: {
+    id: string;
+  };
+}
 
 const router = createEdgeRouter<NextRequest, RequestContext>();
 dbConnect();
 
-router.get(allRooms);
-router.post(newRoom);
+router.get(getRoomDetails);
+router.put(updateRoom);
 
 export async function GET(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
 
-export async function POST(request: NextRequest, ctx: RequestContext) {
+export async function PUT(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
--- backend/controllers/roomController.ts
+++ backend/controllers/roomController.ts
@@ -1,16 +1,76 @@
 import { NextRequest } from "next/server";
 import Room from "../models/room";
 
+interface IParams {
+  params: { id: string };
+}
+
+//GET all rooms /api/rooms
 export const allRooms = async (req: NextRequest) => {
   const rooms = await Room.find();
   return Response.json({ sucess: true, rooms });
 };
 
+//POST Create room /api/rooms
 export const newRoom = async (req: NextRequest) => {
   const body = await req.json();
   const room = await Room.create(body);
   return Response.json({
     Success: true,
+    room,
+  });
+};
+
+// GET room details /api/rooms/:id
+export const getRoomDetails = async (req: NextRequest, { params }: IParams) => {
+  const room = await Room.findById(params.id);
+  if (!room) {
+    return Response.json({ message: "Room not found" }, { status: 404 });
+  }
+  return Response.json({ success: true, room });
+};
+
+//UPDATE a room /api/rooms/:id
+
+export const updateRoom = async (req: NextRequest, { params }: IParams) => {
+  let room = await Room.findById(params.id);
+  const body = await req.json();
+
+  if (!room) {
+    return Response.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+
+  room = await Room.findByIdAndUpdate(params.id, body, { new: true });
+
+  return Response.json({
+    success: true,
+    room,
+  });
+};
+
+export const deleteRoom = async (req: NextRequest, { params }: IParams) => {
+  let room = await Room.findById(params.id);
+  // const body = await req.json();
+
+  if (!room) {
+    return Response.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+
+  room = await Room.findByIdAndDelete(params.id);
+  console.log(room);
+
+  return Response.json({
+    success: true,
     room,
   });
 };
