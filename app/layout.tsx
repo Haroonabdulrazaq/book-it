43a700f14d342443a3707db032b36ffbc2083cd1
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2024-02-12 11:05:34 +0100

    Add bootstrap,Headerand Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GlobalProvider } from "./GlobalProvider";
+import Head from "./head";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head />
+      <body className={inter.className}>
+        <GlobalProvider>
+          <Header />
+          {children}
+          <Footer />
+        </GlobalProvider>
+
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></Script>
+        <Script src="https://kit.fontawesome.com/9edb65c86a.js"></Script>
+      </body>
     </html>
   );
 }
